a273a7cbc0636cdcc082910e066002c6973f3e9b
Kevin Soules <inceax64@gmail.com>
2016-08-24 16:05:42 +0100

    Add multiple options on process.py

--- sources/scope_instrumentation/process.py
+++ sources/scope_instrumentation/process.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import argparse
 import itertools
 from scipy import signal
+import matplotlib.ticker as tkr
 
 def remove_lower_half(x):
     if x < 0:
@@ -21,23 +22,42 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     parser.add_argument('FILE', help='File to process')
+    parser.add_argument('--out', default="out.png", help='Output filename')
     parser.add_argument('--quick', action="store_true", help='Draft image')
+    parser.add_argument('--full', default=False, action="store_true", help='Does not half the signal')
+    parser.add_argument('--from', dest="frm", default=0, type=float, help='Display from offset (ms)')
+    parser.add_argument('--to', default=192, type=float, help='Display to offset (ms)')
+    parser.add_argument('--input_data_duration', default=192, type=int, help='Duration (in ms) of the input data')
+    parser.add_argument('--decimation', default=3, type=int, help='Decimation of the input data')
     args = parser.parse_args()
+
+    data_duration = args.to - args.frm
+    
 
     data = numpy.load(args.FILE)
     print("Numpy file loaded")
 
-    deci = 3
-    data = signal.decimate(data, deci)
+    data_size = len(data)
+
+    nb_idx_per_ms = data_size / args.input_data_duration
+    from_idx = int(args.frm * nb_idx_per_ms)
+    to_idx = int(args.to * nb_idx_per_ms)
+
+    print("len bef:", data_size)
+    data = data[from_idx:to_idx]
+    print("len aft:", len(data))
+    
+    data = signal.decimate(data, args.decimation)
     data_size = len(data)
 
     #Center to 0
     avg = numpy.average(data)
     data = data - avg
 
-    print("Halfing signal")
-    vfunc = numpy.vectorize(remove_lower_half)
-    data = vfunc(data)
+    if not args.full:
+        print("Halfing signal")
+        vfunc = numpy.vectorize(remove_lower_half)
+        data = vfunc(data)
 
     #Convert to a readable unit
     data = data * 10*1.057031e-01/255*100
@@ -52,15 +72,20 @@ if __name__ == "__main__":
 
     dpi = 50
     if not args.quick:
-        dpi = 200    
+        dpi = 220
         ax.set_xlabel('time (ms)')
         ax.set_ylabel('voltage (V)')
-        ax.set_xlim(0, 200)
-        ax.set_xticks(numpy.arange(0, 200, 1))
-        ax.set_xticklabels(numpy.arange(0, 200, 1), rotation=45, fontsize=7)
+
+        tick = data_duration / 100
+        
+        ax.set_xlim(0, data_duration)
+        ax.set_xticks(numpy.arange(0, data_duration, tick))
+        ax.set_xticklabels(numpy.arange(args.frm, args.to, tick), rotation=45, fontsize=7)
+        
         ax.grid()
 
 
-    ax.plot(numpy.linspace(0, 192, num=data_size), data)
+    ax.plot(numpy.linspace(0, data_duration, num=data_size), data)
 
-    plt.savefig('out.png', bbox_inches='tight', dpi=dpi)
+    plt.savefig(args.out, bbox_inches='tight', dpi=dpi)
+    print("Output saved in %s" % args.out)
